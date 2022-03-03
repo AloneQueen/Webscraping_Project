d70c4650f35da41c43768a210d57777001c9907b
Angela Spencer <livesimplyspencer@gmail.com>
2022-03-02 18:01:53 -0600

    update application

--- scrape_app.py
+++ scrape_app.py
@@ -1,36 +1,27 @@
 #!/usr/bin/env python
 
-from flask import Flask, json, render_template, request
+from flask import Flask, render_template
 from scrape_func import craigslist_scrape
-import os
+from IPython.display import HTML
 
 #create instance of Flask app
 app = Flask(__name__)
 
+url, df = craigslist_scrape()
+html_table = HTML(df.to_html(justify="center", classes="table table-striped", index=False))
+
 #decorator
 @app.route("/")
-def echo_hello():
+def welcome():
     return render_template('intro.html')
-    #add more info about how to use 
 
 @app.route("/scrape")
 def scrape():
+    return render_template('index.html', data=url)
 
-    func_output = craigslist_scrape()
-
-    return render_template('index.html', data=func_output)
-
-# @app.route("/scrape/all")
-# def all():
-#     json_url = os.path.join(app.static_folder,"","us_gdp.json")
-#     data_json = json.load(open(json_url))
-
-#     data = data_json[1]
-#     #print(data)
-#     year = request.view_args['year']
-
-#     output_data = [x for x in data if x['date']==year]
-#     return render_template('index.html',data=output_data)
+@app.route("/scrape/all")
+def all():
+    return render_template('index.html', data=html_table)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- scrape_func.py
+++ scrape_func.py
@@ -1,23 +1,51 @@
 #!/usr/bin/env python
 
-from bs4 import BeautifulSoup as bs
 from splinter import Browser
 from webdriver_manager.chrome import ChromeDriverManager
+from scrapy import Selector
+import re
 import pandas as pd
 
 def craigslist_scrape(url='https://stlouis.craigslist.org/search/msa'):
+    #create pathway and browser
     executable_path = {'executable_path':ChromeDriverManager().install()}
-
-    browser = Browser('chrome', **executable_path, headless=False)
-
-    #direct browser to home page
+    browser = Browser('chrome', **executable_path, headless=True)
     browser.visit(url)
 
-    html = bs(browser.html, 'html.parser')
-
     # Create a Selector selecting html as the website HTML
-    #sel = Selector(text=html)
+    sel = Selector(text=browser.html)
+    #pull results from the class containint craigslist listings
+    results = sel.xpath('//li[@class="result-row"]')
+    
+    #list to hold scraped info
+    craigslist_data = []
 
-    return html
+    for i,result in enumerate(results):
+        #limit results to first 50
+        if i < 51:
+            try:
+                link = result.xpath('./a/@href').get()
+                date = result.xpath('./div/time/@datetime').get()
+                listing_no = result.xpath('./div/h3/a/@data-id').get()
+                title = result.xpath('./div/h3/a/text()').get()
+                price = result.xpath('./div/span[@class="result-meta"]/span[@class="result-price"]/text()').get()
+                location = result.xpath('./div/span[@class="result-meta"]/span[@class="result-hood"]/text()').get()
+                #convert location to string without parentheses and extra spaces
+                regex_location = re.findall(r'\((.*)\s\s', location)
+                #append above info to a diction 
+                craigslist_dict = {'title':title,
+                                   'listing_date':date,
+                                   'listing_number':listing_no, 
+                                   'price':price,
+                                   'location':regex_location[0], 
+                                   'listing_url':link}
+                #append dictionary to list
+                craigslist_data.append(craigslist_dict)
+            except Exception as e:
+                print(e)
 
-    browser.quit()
+    #return url browser scraped and dataframe of resulting list of dictionaries
+    return browser.url, pd.DataFrame(craigslist_data)
+
+    #end and close browser connection
+    browser.quit() 
